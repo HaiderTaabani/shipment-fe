57dba32d5e44bb48f5778ba252d876bebba9afba
YazanAlhalabi <yazanalhalabi1@gmail.com>
2026-02-12 18:59:38 +0200

    complete project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ShipmentGrid } from './shipment-grid/shipment-grid';
+import { CreateShipment } from './create-shipment/create-shipment';
+import { UpdateShipment } from './update-shipment/update-shipment';
+import { Header } from './header/header';
+import { Notification } from './notification/notification';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ShipmentGrid, CreateShipment, UpdateShipment, Header, Notification],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('shipment-fe');
